58c8a032d9c5b4152316e9e06170f04d1eb97f6b
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2022-10-27 19:31:16 +0300

    add some  styless delete unnecessary components

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import IsFinished from '../IsFinished/IsFinished';
-import IsImportant from '../IsImportant/IsImportant';
 
 import './TodoCard.css'
 
-function TodoCard({ todo, ...props }) {
+function TodoCard({ todo }) {
 
     return (
         <div key={todo?.id} className='todo-card'>
-            <IsFinished id={todo?.id} checked={todo?.isFinished} />
-            <IsImportant id={todo?.id} isImportant={todo?.isImportant} />
-            <div>ID : {todo?.id}</div>
+            <p className='todo-card--number'>{todo?.id}</p>
             <p className='todo-card--text'>{todo?.text}</p>
-            <p className='todo-card--deeadline'>Deadline : {todo?.deadline}</p>
-
         </div>
     );
 
