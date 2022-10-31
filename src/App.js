e48989c5bf095cf1d0c944ccfe53ec040010c848
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2022-10-31 22:14:04 +0300

    add localeStorage

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import AddForm from "./components/AddForm/AddForm";
 import AddButton from "./components/AddButton/AddButton";
 
 function App() {
-  const [todoItems, setTodoItems] = useState(todos);
+  const [todoItems, setTodoItems] = useState(
+    JSON.parse(localStorage.getItem("Todos")) || []
+  );
+
   const [activeClass, setActiveClass] = useState(true);
 
   const addTodo = (todo) => {
     todo.id = todoItems.length + 1;
     const tmpArr = [...todoItems, todo];
+    localStorage.setItem("Todods", JSON.stringify(tmpArr));
+    console.log(JSON.parse(localStorage.getItem("Todods")));
     setTodoItems(tmpArr);
   };
 
