abfb038b9feb1d40ff9bd5934567ce2efd5753c4
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2022-12-27 21:46:32 +0300

    refactor delete emty lines

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import './Popup.css';
-import PropTypes from 'prop-types';
-
 import { createPortal } from 'react-dom';
+
+import PropTypes from 'prop-types';
 
 import Button from '../Button/Button';
 
-
+import './Popup.css';
 
 function Popup({ onConfirmDelete, setDeleteTaskId }) {
-
     const popup = document.getElementById('popup');
 
     return (
